655b403a28f6a7720545fa9b5a0e2b23eb399196
Solonetskayairina <Solonetskayairina@ukr.net>
2023-09-26 15:06:52 +0300

    add files

---
+++ js/index.js
@@ -0,0 +1,15 @@
+    let a= parseInt(prompt("Enter number a"));
+    let b= parseInt(prompt("Enter number b"));
+    let c= 0;
+
+    c = (a + b);
+    alert(a + '+' + b + '=' + c);
+
+    c = (a - b);
+    alert(a + '-' + b + '=' + c);
+
+    c = (a * b);
+    alert(a + '*' + b + '=' + c);
+
+    c = (a / b);
+    alert(a + '/' + b + '=' + c);
