4b3b6167f0927aca2a73da08dcb85b2c18bd8853
Solonetskayairina <Solonetskayairina@ukr.net>
2023-09-26 16:37:08 +0300

    changed js

--- js/index.js
+++ js/index.js
@@ -1,15 +1,40 @@
-    let a= parseInt(prompt("Enter number a"));
-    let b= parseInt(prompt("Enter number b"));
+    // let a= parseInt(prompt("Enter number a"));
+    // let b= parseInt(prompt("Enter number b"));
+    // let c= 0;
+    //
+    // c = (a + b);
+    // alert(a + '+' + b + '=' + c);
+    //
+    // c = (a - b);
+    // alert(a + '-' + b + '=' + c);
+    //
+    // c = (a * b);
+    // alert(a + '*' + b + '=' + c);
+    //
+    // c = (a / b);
+    // alert(a + '/' + b + '=' + c);
+
+    let a= prompt("Enter number a");
+    let b= prompt("Enter number b");
     let c= 0;
 
-    c = (a + b);
+    if(isNaN(a)){
+        alert('You can only enter numbers');
+    }else if (isNaN(b)){
+        alert('You can only enter numbers');
+    }
+
+    c = (Number(a) + Number(b));
     alert(a + '+' + b + '=' + c);
 
-    c = (a - b);
+    c = (Number(a) - Number(b));
     alert(a + '-' + b + '=' + c);
 
-    c = (a * b);
+    c = (Number(a) * Number(b));
     alert(a + '*' + b + '=' + c);
 
-    c = (a / b);
+    c = (Number(a) / Number(b));
     alert(a + '/' + b + '=' + c);
+
+
+
